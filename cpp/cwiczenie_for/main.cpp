da077c31ddfb65f27b86c95c314f73dbfe674dc6
PatrykBednarczyk <bednarczyk.patryk@poczta.onet.pl>
2024-01-18 13:53:25 +0000

    studentmachine send

---
+++ cpp/cwiczenie_for/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+for(int x; x<10; x++){
+cout<<x<<endl;
+}
+
+	return 0;
+}
---
+++ cpp/cwiczenie_if/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+cout<<"Podaj wiek"<<endl;
+cin>>x;
+if(x<18){
+cout<<"jestes niepelnoletni"<<endl;
+}else{
+cout<<"Jestes pelnoletni"<<endl;
+}
+	return 0;
+}
