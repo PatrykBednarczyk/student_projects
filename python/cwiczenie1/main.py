1d5c3cccbd6db70bb8b4541b2b5e3cd30d175b46
PatrykBednarczyk <bednarczyk.patryk@poczta.onet.pl>
2024-04-11 13:06:22 +0000

    studentmachine send

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,14 @@
+print ("witaj swiecie")
+
+x=float (input("podaj wartosc x"))
+print (x)
+y=float (input("podaj wartosc y"))
+print (x+y)
+if x>10:
+	print ("x jest wieksze od 10")
+elif x==10 and y==40:
+	print ("x jest rowne 10")
+else:
+	print ("x jest mniejszy od 10")
+for z in range(0,10):
+	print (z)
